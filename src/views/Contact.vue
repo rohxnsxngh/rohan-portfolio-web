9e9a0254d87a3a4afb1a555a6e880d5a414f86d2
rohxnsxngh <98181667+rohxnsxngh@users.noreply.github.com>
2023-12-26 09:31:10 -0600

    adjusting font, ui, lights

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="text-white overscroll-none saturate-100 subpixel-antialiased bg-primary-content skeleton border border-base-300 opacity-65 text-4xl rounded-lg opacity-90 md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl"
+    class="text-white overscroll-none saturate-100 subpixel-antialiased bg-primary-content skeleton border border-base-300 opacity-60 text-4xl rounded-lg opacity-90 md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl"
     id="contact-panel"
   >
     <div class="flex flex-col">
--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="text-white overscroll-none saturate-100 subpixel-antialiased bg-primary-content border border-base-300 opacity-65 text-4xl rounded-lg opacity-90"
+    class="text-white overscroll-none saturate-100 subpixel-antialiased bg-primary-content border border-base-300 opacity-60 text-4xl rounded-lg opacity-90"
     id="experience-panel"
   >
   <div class="flex flex-col">
