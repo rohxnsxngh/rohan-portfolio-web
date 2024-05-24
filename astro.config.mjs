fbdb8752757f7a126e08e005135b7c43c1e8eeae
rohxnsxngh <164418804+rohxnsxngh@users.noreply.github.com>
2024-05-24 11:05:43 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server",
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
