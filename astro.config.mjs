2d7d205d52721bc63b79b53bc96b1e7410e16047
rohxnsxngh <164418804+rohxnsxngh@users.noreply.github.com>
2025-03-09 10:32:16 -0400

    feat: updating config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-import vercel from '@astrojs/vercel';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
